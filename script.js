72d9ac61babe8abc78fa73c17ec81df3689b94d1
Ngomeli <joshuangomeli@gmail.com>
2025-11-25 16:07:08 +0300

    troubleshooting fullscreen feature

--- script.js
+++ script.js
@@ -251,3 +251,23 @@ document.addEventListener("fullscreenchange", () => {
     fullscreenBtn.textContent = "Exit Fullscreen";
   }
 });
+
+function enterFullscreen() {
+  const el = document.documentElement;
+
+  if (el.requestFullscreen) el.requestFullscreen();
+  else if (el.webkitRequestFullscreen) el.webkitRequestFullscreen();
+  else if (el.msRequestFullscreen) el.msRequestFullscreen();
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) document.exitFullscreen();
+  else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+  else if (document.msExitFullscreen) document.msExitFullscreen();
+}
+
+if (!document.fullscreenElement) {
+  enterFullscreen();
+} else {
+  exitFullscreen();
+}
